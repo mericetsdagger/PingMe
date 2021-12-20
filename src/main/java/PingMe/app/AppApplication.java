6ff3d35de201ed523435b72d7c8b7f3f97c45e7b
mericetsdagger <45637254+mericetsdagger@users.noreply.github.com>
2021-12-20 18:11:46 +0100

    Add files via upload
    init

---
+++ src/main/java/PingMe/app/AppApplication.java
@@ -0,0 +1,13 @@
+package PingMe.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
