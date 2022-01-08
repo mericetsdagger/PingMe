0f8d2f929ad9b4b55421065b082e0ab4dec15ad7
mericetsdagger <papaj.jacek@gmail.com>
2022-01-08 13:46:03 +0100

    zapis prac backend

--- src/main/java/PingMe/app/AppApplication.java
+++ src/main/java/PingMe/app/AppApplication.java
@@ -1,7 +1,12 @@
 package PingMe.app;
 
+import PingMe.app.enumeration.Status;
+import PingMe.app.model.Server;
+import PingMe.app.repo.ServerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppApplication {
@@ -10,4 +15,13 @@ public class AppApplication {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(ServerRepo serverRepo){
+		return args -> {
+			serverRepo.save(new Server(null, "192.168.1.168", "Windows", "16 GB", "PC", "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.1.1", "Linux", "16 GB", "Notebook", "http://localhost:8080/server/image/server2.png", Status.SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.1.2", "DB", "16 GB", "DB", "http://localhost:8080/server/image/server3.png", Status.SERVER_UP));
+
+		};
+	}
 }
