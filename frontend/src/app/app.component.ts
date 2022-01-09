bddc0d2cdf825f4b7ef361fea3f475467457ad71
mericetsdagger <papaj.jacek@gmail.com>
2022-01-09 13:27:10 +0100

    dodanie do frontu serwerow, obrazkow, usuniecie bledow

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -13,20 +13,22 @@ import { ServerService } from './service/server.service';
 export class AppComponent implements OnInit {
 
   appState$: Observable<AppState<CustomResponse>>;
+  readonly DataState = DataState;
+
   constructor(private serverService: ServerService) {
-    
+
   }
 
   ngOnInit(): void {
     this.appState$ = this.serverService.servers$
-    .pipe(
-      map(response => {
-        return{ dataState: DataState.LOADED_STATE, appData: response}
-      }),
-      startWith({ dataState: DataState.LOADING_STATE }),
-      catchError((error: string) => {
-        return of({ dataState: DataState.ERROR_STATE, error })
-      })
-    );
+      .pipe(
+        map(response => {
+          return { dataState: DataState.LOADED_STATE, appData: response }
+        }),
+        startWith({ dataState: DataState.LOADING_STATE }),
+        catchError((error: string) => {
+          return of({ dataState: DataState.ERROR_STATE, error })
+        })
+      );
   }
 }
