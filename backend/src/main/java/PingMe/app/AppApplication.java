8c165bd5fcadec0c1919cda4d5dd5936fb9c2615
mericetsdagger <papaj.jacek@gmail.com>
2022-01-10 18:28:12 +0100

    sprzatanie i poprawka z backendu niedzialajacego obrazka po dodaniu servera

--- backend/src/main/java/PingMe/app/AppApplication.java
+++ backend/src/main/java/PingMe/app/AppApplication.java
@@ -26,7 +26,6 @@ public class AppApplication {
 			serverRepo.save(new Server(null, "192.168.1.168", "Windows", "16 GB", "PC", "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
 			serverRepo.save(new Server(null, "192.168.1.1", "Linux", "16 GB", "Notebook", "http://localhost:8080/server/image/server2.png", Status.SERVER_UP));
 			serverRepo.save(new Server(null, "192.168.1.2", "DB", "16 GB", "DB", "http://localhost:8080/server/image/server3.png", Status.SERVER_UP));
-
 		};
 	}
 
--- backend/src/main/java/PingMe/app/service/implementation/ServerServiceImpl.java
+++ backend/src/main/java/PingMe/app/service/implementation/ServerServiceImpl.java
@@ -69,6 +69,6 @@ public class ServerServiceImpl implements ServerService {
 
     private String setServerImageUrl() {
         String[] imageNames = { "server1.png", "server2.png", "server3.png"};
-        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image" + imageNames[new Random().nextInt(3)]).toUriString();
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image/" + imageNames[new Random().nextInt(3)]).toUriString();
     }
 }
