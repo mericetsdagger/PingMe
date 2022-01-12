c68cbd0fa372aced3efb564f893a256cd1b13352
mericetsdagger <papaj.jacek@gmail.com>
2022-01-12 19:08:19 +0100

    naprawa bledu kompilacji oraz niedzialajacych powiadomien

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { NotifierModule } from 'angular-notifier';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    NotifierModule,
     FormsModule
   ],
   providers: [],
