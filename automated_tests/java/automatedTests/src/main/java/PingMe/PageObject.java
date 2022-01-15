b0d849ff714fe9694d0aaf922cd2f61fd4a0bbe3
mericetsdagger <papaj.jacek@gmail.com>
2022-01-15 19:01:32 +0100

    testy automatyczne w javie - pagefactory + selenium do pom

---
+++ automated_tests/java/automatedTests/src/main/java/PingMe/PageObject.java
@@ -0,0 +1,17 @@
+package PingMe;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class PageObject {
+
+    protected WebDriver driver;
+    protected WebDriverWait waitBase;
+
+    public PageObject(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+        this.waitBase = new WebDriverWait(driver, 60);
+    }
+}
